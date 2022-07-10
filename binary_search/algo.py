58c5c5081bd76331f43228b8a676624c8de30411
anup.y <anup.y@BMMDVANUPY.local>
2022-07-10 21:12:47 +0530

    [python_worksheet] added binary_search algo.py

--- binary_search/algo.py
+++ binary_search/algo.py
@@ -2,3 +2,30 @@
 
 
 print("Python  script for binary search.")
+
+def binary_search(arr, n ,k):
+    start = 0
+    end = n-1
+    
+
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] < k:
+            start = mid +1
+        elif arr[mid] > k:
+            end = mid - 1
+        else:
+            return mid
+
+
+    return None
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr=list(map(int, input().split(' ') ))
+    
+    k = int(input())
+
+    ans=binary_search(arr, n, k)
+    print("Found at: ",ans)
